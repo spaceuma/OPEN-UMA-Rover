1949cf38af741f1c9aba74af4be1790370f3c84e
Danil Pavlenko <94115082+Danilrivero@users.noreply.github.com>
2022-10-05 10:07:11 +0200

    Nodo para uso con ROS2 Humble
    Nodo de Python junto con el archivo de Arduino para su uso con ROS2 Humble.

---
+++ ROS2UMARov.py
@@ -0,0 +1,127 @@
+#Python API for ROS2s
+import rclpy 
+from rclpy.node import Node
+
+#Thread Library for rclpy spin
+import threading
+
+# Type of messages published on different topics
+#from std_msgs.msg import Float32
+from geometry_msgs.msg import Twist
+import serial #To use install: pip install pyserial
+import time
+import numpy as np
+import math
+
+# Initialization of the array where the speeds received from the topics will be stored
+my_data = [0, 0]
+
+# Creation of the serial object
+port = '/dev/ttyACM0'
+baudrate = 115200
+bytesize = serial.EIGHTBITS
+parity = serial.PARITY_NONE
+stopbits = serial.STOPBITS_ONE
+timeout = 0
+xonxoff = False
+rtscts = False
+write_timeout = 0
+
+# Starting serial communication
+arduino = serial.Serial(port, baudrate, bytesize, parity, stopbits, timeout, xonxoff, rtscts, write_timeout)
+
+# 1 second break waiting for Arduino and Raspberry to connect
+time.sleep(1)
+
+# Principal node
+class open_uma_rov_node(Node):
+
+    def __init__(self):
+        super().__init__('rov_node')
+        #Wheels distance
+        #self.declare_parameter('L',0.258)
+        #Create subscription
+        self.speed = self.create_subscription(Twist,'cmd_vel',self.Callback,10)
+
+    #Recibe la informacion de velocidad    
+    def Callback(self, msg):
+        self.v = msg.linear.x
+        self.w = msg.angular.z
+        # Calculate velocities for each wheel
+        L = 0.258 #Wheels distance
+        my_data[0] = ((1.0 * self.v - self.w * L / 2)*100) #Left
+        my_data[1] = ((1.0 * self.v + self.w * L / 2)*100) #Right
+        #La informacion que se debe recibir de Nav2 es en m/s pero el archivo OpenUMARov.ino trabaja en cm/s.
+        #Debido a ello se recibe una consigna en m/s, que al ser operada es multiplicada por 100 para que la v de cada rueda sea cm/s.
+        #La información de Nav2 se recibe en formato de velocidad {x,y,theta}, donde se limita y=0 por ser vehiculo no omnidireccional.
+        print("[Velocity command received]" + "\n")                                          
+        print("Linear velocity = " + str(self.v) + "\n")
+        print("Angular velocity = " + str(self.w) + "\n")
+        print("Wheel Velocity Left: " + str(my_data[0]) + "\n")
+        print("Wheel Velocity Right: " + str(my_data[1]) + "\n")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    rov_node = open_uma_rov_node()
+    #Create a thread to constantly spin the node while constantly executing the loop at the same time
+    thread = threading.Thread(target=rclpy.spin, args=(rov_node, ), daemon=True)
+    thread.start()
+
+    # Beginning of ROS main loop
+    while rclpy.ok():
+
+        print("---ROS2 UMA ROV---")
+
+        # Storing in two different variables data from the topic
+        speed_1 = my_data[0]
+        speed_2 = my_data[1]
+
+        print("First speed is: " + str(speed_1) + "\n")
+
+        print("Seconds speed is: " + str(speed_2) + "\n")
+
+        # Calculation of the pulses
+        speed_1_pulses = speed_1 * (0.01 / (0.03 * (37.7 / 360)))
+
+        speed_2_pulses = speed_2 * (0.01 / (0.03 * (37.7 / 360)))
+
+        # Rounding pulses values
+        speed_1_pulses_rounded = int(math.ceil(speed_1_pulses))
+
+        speed_2_pulses_rounded = int(math.ceil(speed_2_pulses))
+
+        # Creating two byte arrays, one for each data that will be sent through serial port, where each speed
+        # will be converted to an integer of 16 bits: this means that each speed will occupy 2 positions in the array,
+        # sending through serial port a total of 4 bytes
+        byte_array_1 = np.array([speed_1_pulses_rounded], dtype=np.int16)
+
+        byte_array_2 = np.array([speed_2_pulses_rounded], dtype=np.int16)
+
+        # Cleaning the buffer before sending data
+        arduino.flush()
+
+        # Sending the first speed
+        arduino.write(byte_array_1)
+
+        # 0.1 second pause
+        time.sleep(0.1)
+
+        # Sending the second speed
+        arduino.write(byte_array_2)
+
+        # 0.2 seconds pause
+        time.sleep(0.2)
+
+        # Buffer cleaning
+        arduino.flush()
+
+    rov_node.destroy_node()
+    rclpy.shutdown()
+    #Closes Thread activity
+    thread.join()
+
+# Catching possible interruptions which can be thrown by rospy.sleep() when Ctrl-C is pressed or
+# the Node is otherwise shutdown
+if __name__ == '__main__':
+    main()
---
+++ openUMARov/openUMARov.ino
@@ -0,0 +1,536 @@
+// Código Arduino para control en posición y velocidad y lectura de corrientes de los motores de un rover.
+
+//Librerias
+#include <TimerOne.h>
+#include<avr/interrupt.h>
+#include<avr/io.h>
+#include <SoftwareSerial.h>
+
+//Definicion de los pines del encoder 1
+#define encoder1PinA  2
+#define encoder1PinB  3
+
+//Definicion de los pines del encoder 2
+#define encoder2PinA 21
+#define encoder2PinB 20
+
+//Definicion de los pines donde esta conectado la señal PWM en arduino y puentes H
+#define pinPWM1 9
+#define pinPWM2 6
+
+//Definicion de los pines para la direccion de las ruedas (PWM)
+#define  dirPWM1 8
+#define  dirPWM2 7
+
+
+//Defincion de los pines donde se encuentran los sensores de corriente.
+const int sensorCurrent1 = A1;
+const int sensorCurrent2 = A2;
+
+
+//Definicion de las constantes para el PID 1
+#define kp1 3
+#define ki1 30
+#define kd1 0
+
+//Definicion de las constantes para el PID 2
+#define kp2 3
+#define ki2 30
+#define kd2 0
+
+
+//Corriente -- variable para lectura de corriente
+float lectura_analog1;
+float lectura_analog2;
+
+
+//PWM -- variable para designar valor al PWM
+byte valorPWM1;
+byte valorPWM2;
+
+
+
+//Variables para interrupciones
+//Guarda el numero de pulsos de la rueda 1
+float pulsos1;
+//Guarda el numero de pulsos de la rueda 1, 10 ms antes que es el tiempo al cual esta configurada la interrupcion
+float pulsos1Ant;
+//Diferencia de pulsos que se han dado en los 10 ms (velpulsos=pulsos-pulsosAnt)
+float velpulsos1;
+//Guarda el numero de pulsos de la rueda 2
+float pulsos2;
+//Guarda el numero de pulsos de la rueda 2, 10 ms antes que es el tiempo al cual esta configurada la interrupcion
+float pulsos2Ant;
+//Diferencia de pulsos que se han dado en los 10 ms (velpulsos=pulsos-pulsosAnt)
+float velpulsos2;
+
+
+
+//Designacion del Tiempo interrupcion temporizada 10ms
+unsigned long tiempoint = 10000;
+
+
+//Variables globales para PID 1
+int volatile sn1;
+int volatile en1;
+int volatile enOld1;
+int volatile mn1;
+
+
+//Variables globales para PID 2
+int volatile sn2;
+int volatile en2;
+int volatile enOld2;
+int volatile mn2;
+
+//Variables para establecer limites minimo y maximo de los PID (efecto wind-up)
+int volatile snmin;
+int volatile snmax;
+
+//Referencia para los PID
+//Estas referencia son el numero de pulsos que se deben de dar para mantener una velocidad constante
+int volatile ref1;
+int volatile ref2;
+
+//Para la recepcion de las velocidades aportadas por el nodo de ROS
+
+//En estos dos buffers se guardan los 4 bytes que llegan por puerto serie, los 2 primeros se guardan en buffIn_1 y se corresponden 
+//a la velocidad de la rueda 1, y los 2 siguientes se guardan en buffIn_2 y se corresponden a la velocidad de la rueda 2.
+byte buffIn_1 [2] = {0, 0};
+byte buffIn_2 [2] = {0, 0};
+//Variable auxiliar para juntar los 2 bytes para obtener la referencia de velocidad de las ruedas 1 y 2 respectivamente.
+byte long auxi1;
+byte long auxi2;
+int g;
+
+
+
+void setup()
+{
+
+  auxi1 = 0;
+  auxi2 = 0;
+  ref1 = 0;
+  ref2 = 0;
+  
+  //Inicializacion de las variables
+  pulsos1 = 0;
+  pulsos1Ant = 0;
+  velpulsos1 = 0;
+  pulsos2 = 0;
+  pulsos2Ant = 0;
+  velpulsos2 = 0;
+
+
+  //Designacion del valor inicial del PWM (0 -- 0%, 64 -- 25%, 127 -- 50%, 191 -- 75%, 255 -- 100%)
+  valorPWM1 = 0;
+  valorPWM2 = 0;
+
+
+  //Inicializacion de las variables de los PID
+  sn1 = 0;
+  en1 = 0;
+  enOld1 = 0;
+  mn1 = 0;
+  sn2 = 0;
+  en2 = 0;
+  enOld2 = 0;
+  mn2 = 0;
+
+  //Establecimiento de unos limites ya que el motor tiene una velocidad maxima dada por sus caracteristicas.
+  snmin= -600;
+  snmax= 600;
+
+
+
+  //INTERRUPCIONES EXTERNAS -- ENCODERS -- CONTADOR DE PULSOS
+  //Configuracion como pines de entrada los pines donde estan los canales A y B del encoder 1.
+  pinMode(encoder1PinA, INPUT);
+  pinMode(encoder1PinB, INPUT);
+
+  //Configuracion como pines de entrada los pines donde estan los canales A y B del encoder 2.
+  pinMode(encoder2PinA, INPUT);
+  pinMode(encoder2PinB, INPUT);
+
+
+  //Configuracion de las interrupciones del encoder 1, las cuales se han configurado para que salte en disparo de subida y bajada.
+  attachInterrupt(0, doEncoder1A, CHANGE);
+  attachInterrupt(1, doEncoder1B, CHANGE);
+
+  //Configuracion de las interrupciones del encoder 2, las cuales se han configurado para que salte en disparo de subida y bajada.
+  attachInterrupt(2, doEncoder2A, CHANGE);
+  attachInterrupt(3, doEncoder2B, CHANGE);
+
+
+  //Pines de lectura de corriente como entradas.
+  pinMode(sensorCurrent1, INPUT);
+  pinMode(sensorCurrent2, INPUT);
+
+
+
+  //INTERRUPCION TIEMPO (TIMER1) -- CALCULO VELOCIDAD
+  //Configuracion interrupcion temporizada para calculo de la velocidad.
+  //Intervalo de tiempo de disparo en microsegundos.
+  Timer1.initialize(tiempoint);
+
+  //Attach de la interrupcion con el servicio de gestion
+  Timer1.attachInterrupt(ISR_cVelocidad);
+
+
+
+
+  //PWM -- TRANSMITE POTENCIA Y DIRECCION
+  //Configuracion como salida los pines de direccion de las ruedas.
+  pinMode(dirPWM1, OUTPUT);
+  pinMode(dirPWM2, OUTPUT);
+  //Configuración de velocidad inicial deseada de las ruedas en funcion del valorPWM
+  analogWrite(pinPWM1, valorPWM1);
+  analogWrite(pinPWM2, valorPWM2);
+
+
+  //Habilitacion de interrupciones
+  //Habilita todas las interrupciones a nivel de programacion del microcontrolador.
+  sei();
+  //Habilita todas las interrupciones a nivel de programación de arduino.
+  interrupts();
+  //Inicializacion y Configuracion de la velocidad del puerto serie, la cual se ha establecido a 9600 bits/s.
+  Serial.begin(115200);
+  // Se definen los milisegundos maximos de espera de datos desde el puerto serie.
+  Serial.setTimeout(1000);
+  while (!Serial) {
+    ; // Espera hasta que se conecte el puerto serie.
+  }
+
+}
+
+void loop()
+{
+  //Espera hasta que halla alguna informacion disponible en el puerto serie.
+  if (Serial.available())
+  {
+    //La funcion serial.readbytes lee datos del buffer serie y los guarda en dos variables buffer que tienen una capacidad de 2 bytes cada una.      
+    Serial.readBytes(buffIn_1, 2);
+    Serial.readBytes(buffIn_2, 2);
+    // Espera de un segundo.
+    delay(100);
+
+    //Limpieza del buffer
+    Serial.flush();
+
+    //En la variable auxi1 guardamos los 2 bytes ubicados en buffIn_1, los cuales al agruparlos se corresponden con los pulsos que se tienen que dar
+    //para tener la referencia de la velocidad seleccionada para el motor 1.
+    auxi1 = (byte)buffIn_1[1] << 8;
+    auxi1 += (byte)buffIn_1[0];
+    //En la variable ref guardamos la variable auxi la cual es de tipo byte y la convertimos a tipo entero de 32 bits.
+    ref1 = (long int)auxi1;
+    //En la variable auxi2 guardamos los 2 bytes ubicados en buffIn_2, los cuales al agruparlos se corresponden con los pulsos que se tienen que dar
+    //para tener la referencia de la velocidad seleccionada para el motor 2.
+    auxi2 = (byte)buffIn_2[1] << 8;
+    auxi2 += (byte)buffIn_2[0];
+    //En la variable ref guardamos la variable auxi la cual es de tipo byte y la convertimos a tipo entero de 32 bits.
+    ref2 = (long int)auxi2;
+    //Lectura de la corriente de los motores
+    lectura_analog1 = analogRead(sensorCurrent1);
+    lectura_analog2 = analogRead(sensorCurrent2);
+
+    //Envio de datos al nodo de ROS (escritura por puerto serie)
+    Serial.println(pulsos1);
+   
+    Serial.println(pulsos2);
+    
+    Serial.println(velpulsos1);
+    
+    Serial.println(velpulsos2);
+    
+    Serial.println(ref1);
+   
+    Serial.println(ref2);
+      
+    Serial.flush();
+
+    // Se limpia el buffer, abriendo y cerrando el puerto serie.
+    Serial.end();
+    Serial.begin(115200);
+    // Espera de un segundo.
+    delay(100);
+
+  } 
+  // Si no hay ninguna informacion disponible para ser recibida, se limpia el buffer y se espera hasta que llegue algun dato.
+  else {
+
+    // Se limpia el buffer, abriendo y cerrando el puerto serie.
+    Serial.end();
+    Serial.begin(115200);
+    // Espera de un segundo.
+    delay(100);
+  }
+
+
+}
+
+
+
+//Interrupciones encoder 1
+//Canal A
+void doEncoder1A()
+{
+  sei();
+  interrupts();
+
+  if (bit_is_set(PINE, 4))
+  {
+    if (bit_is_set(PINE, 5))
+    {
+      pulsos1--;
+    }
+    else
+    {
+      pulsos1++;
+    }
+  }
+  else
+  {
+    if (bit_is_set(PINE, 5))
+    {
+      pulsos1++;
+    }
+    else
+    {
+      pulsos1--;
+    }
+  }
+}
+
+//Canal B
+void doEncoder1B()
+{
+  sei();
+  interrupts();
+  if (bit_is_set(PINE, 5))
+  {
+    if (bit_is_set(PINE, 4))
+    {
+      pulsos1++;
+    }
+    else
+    {
+      pulsos1--;
+    }
+  }
+  else
+  {
+    if (bit_is_set(PINE, 4))
+    {
+      pulsos1--;
+    }
+    else
+    {
+      pulsos1++;
+    }
+  }
+}
+
+//Interrupciones encoder 2
+//Canal A
+void doEncoder2A()
+{
+  sei();
+  interrupts();
+  if (bit_is_set(PIND, 0))
+  {
+    if (bit_is_set(PIND, 1))
+    {
+      pulsos2++;
+    }
+    else
+    {
+      pulsos2--;
+    }
+  }
+  else
+  {
+    if (bit_is_set(PIND, 1))
+    {
+      pulsos2--;
+    }
+    else
+    {
+      pulsos2++;
+    }
+  }
+}
+
+//Canal B
+void doEncoder2B()
+{
+  sei();
+  interrupts();
+  if (bit_is_set(PIND, 1))
+  {
+    if (bit_is_set(PIND, 0))
+    {
+      pulsos2--;
+    }
+    else
+    {
+      pulsos2++;
+    }
+  }
+  else
+  {
+    if (bit_is_set(PIND, 0))
+    {
+      pulsos2++;
+    }
+    else
+    {
+      pulsos2--;
+    }
+  }
+}
+
+
+
+//Interrupcion para obtencion de la velocidad (salta cada 10ms)
+void ISR_cVelocidad()
+{
+  sei();
+  interrupts();
+
+  //Calculo la diferencia de pulsos que se dan en 10 ms en la rueda 1.
+  velpulsos1 = pulsos1 - pulsos1Ant;
+  //Se actualiza el valor de los pulsos que se llevan.
+  pulsos1Ant = pulsos1;
+
+  //Calculo la diferencia de pulsos que se dan en 10 ms en la rueda 2.
+  velpulsos2 = pulsos2 - pulsos2Ant;
+  //Se actualiza el valor de los pulsos que se llevan.
+  pulsos2Ant = pulsos2;
+
+
+  //Calculo del error.
+  en1 = ref1 - (velpulsos1);
+  en2 = ref2 - (velpulsos2);
+  //Llamamiento a las funciones PID.
+  PIDControl1();
+  PIDControl2();
+  //Llamamiento a las funciones que actuan sobre los motores.
+  actuador1();
+  actuador2();
+
+
+}
+
+
+//Funcion PID 1
+void PIDControl1()
+{
+  sei();
+  interrupts();
+
+  //Calculo del sumatorio del error.
+  sn1 = sn1 + en1;
+
+  //Condicionales por si el error sobresale de los limites de velocidad maxima del motor 1.
+  if (sn1 > snmax)
+  {
+    sn1 = snmax;
+  }
+  else if (sn1 < snmin)
+  {
+    sn1 = snmin;
+  }
+  //Calculo del valor PWM1.
+  mn1 = (kp1 * en1) + ((ki1 * sn1) / 100) + (kd1 * (en1 - enOld1));
+  //Se actualiza el error anterior por el actual.
+  enOld1 = en1;
+}
+
+//Funcion para mandar la potencia y direccion al motor 1.
+void actuador1()
+{
+  sei();
+  interrupts();
+  //Condicional por si sobrepasa los limites de velocidad máxima.
+  if (mn1 > 255 || mn1 < -255)
+  {
+    valorPWM1 = 80;
+    //Escribe en los pines del PWM el valor seleccionado.
+    analogWrite(pinPWM1, valorPWM1);
+  }
+  else
+  {
+    //En caso de no superar la velocidad máxima establece el valor de PWM correspondiente a la velocidad objetivo.
+    valorPWM1 = (unsigned char)fabs(mn1);
+    //Escribe en los pines del PWM el valor seleccionado.
+    analogWrite(pinPWM1, valorPWM1);
+  }
+  //Se manda la direccion del PWM1
+  if (mn1 >= 0)
+  {
+    //Si el valor de PWM a establecer es positivo, envia una señal verdadera para sentido positivo.
+    digitalWrite(dirPWM1, HIGH);
+  }
+  else
+  {
+    //En caso contrario si el valor de PWM a establecer es negativo, envia una señal falsa para sentido negativo.
+    digitalWrite(dirPWM1, LOW);
+  }
+}
+
+
+//Funcion PID 2
+void PIDControl2()
+{
+  sei();
+  interrupts();
+  //Calculo del sumatorio del error.
+  sn2 = sn2 + en2;
+
+  //Condicionales por si el error sobresale de los limites de velocidad maxima del motor 2.
+  if (sn2 > snmax)
+  {
+    sn2 = snmax;
+  }
+  else if (sn2 < snmin)
+  {
+    sn2 = snmin;
+  }
+  
+  //Calculo del valor PWM.
+  mn2 = (kp2 * en2) + ((ki2 * sn2) / 100) + (kd2 * (en2 - enOld2));
+  //Se actualiza el error anterior por el actual.
+  enOld2 = en2;
+}
+
+//Funcion para mandar la potencia y direccion al motor 2.
+void actuador2()
+{
+  sei();
+  interrupts();
+  //Condicional por si sobrepasa los limites de velocidad máxima.
+  if (mn2 > 255 || mn2 < -255)
+  {
+    valorPWM2 = 80;
+    //Escribe en los pines del PWM el valor seleccionado.
+    analogWrite(pinPWM2, valorPWM2);
+  }
+  else
+  {
+    //En caso de no superar la velocidad máxima establece el valor de PWM correspondiente a la velocidad objetivo.
+    valorPWM2 = (unsigned char)fabs(mn2);
+    //Escribe en los pines del PWM el valor seleccionado.
+    analogWrite(pinPWM2, valorPWM2);
+  }
+  //Mandamos la direccion del PWM2
+  if (mn2 >= 0)
+  {
+    //Si el valor de PWM a establecer es positivo, envia una señal verdadera para sentido positivo.
+    digitalWrite(dirPWM2, HIGH);
+  }
+  else
+  {
+    //En caso contrario si el valor de PWM a establecer es negativo, envia una señal falsa para sentido negativo.
+    digitalWrite(dirPWM2, LOW);
+  }
+}
